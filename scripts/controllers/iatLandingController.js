e6642169b2d241958f31764c84286cabe4f6f3c1
Adarsh Nanda <adarsh.nanda@t-mobile.com>
2017-02-17 00:31:03 +0530

    ngdisabled

--- scripts/controllers/iatLandingController.js
+++ scripts/controllers/iatLandingController.js
@@ -1,10 +1,11 @@
 var app = angular.module('iat', ['ui.select', 'ngSanitize']);
-	   app.controller('iatLandingController', function (iatLandingService) {
+	   app.controller('iatLandingController', function ($scope, iatLandingService) {
 	var self = this;
 	function init(){
-		self.searchFields = [{}];
+		self.searchFields = [];
 		self.controllerNames = [];
 		self.getControllerName();
+		self.addExtraSearch();
 	}
 	self.getControllerName = function(){
 		iatLandingService.getControllerNames().then(function(response){
@@ -15,6 +16,7 @@ var app = angular.module('iat', ['ui.select', 'ngSanitize']);
 	};
 	self.addExtraSearch = function(){
 		self.searchFields.push({});
+		self.index = self.searchFields.length-1;
 	};
 	init();
 });
--- scripts/services/iatLandingService.js
+++ scripts/services/iatLandingService.js
@@ -11,7 +11,6 @@ var app = angular.module('iat');
                         url: targetUrl,
                     });
 		request.then(function(response){
-			debugger;
 			deffered.resolve(response.data);
 		},function(error){
 			var mock={
