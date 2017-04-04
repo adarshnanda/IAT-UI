63c52899199db90d6f352f094d40f1b8ac024466
Adarsh Nanda <adarsh.nanda@t-mobile.com>
2017-04-04 08:03:03 +0530

    trun on html5 mode

--- scripts/config/ui-states.js
+++ scripts/config/ui-states.js
@@ -1,7 +1,8 @@
-angular.module('iat', ['ui.select', 'ngSanitize','ui.router']).config(function($stateProvider) {
+angular.module('iat', ['ui.select', 'ngSanitize','ui.router']).config(function($stateProvider, $locationProvider) {
 	$stateProvider
     .state('search', {
       url: "/search",
       templateUrl: "scripts/views/search.html"
-    })
+    });
+    $locationProvider.html5Mode(true);
 });
