b36f624cb981915dfdd54e28cee272e5d3ec8c92
Adarsh Nanda <adarsh.nanda@t-mobile.com>
2017-06-12 20:27:28 +0530

    coverage report

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -10,6 +10,21 @@ module.exports = function (config) {
     	browsers: [process.env['browser'] || 'Chrome'],
     	singleRun: false,
    		browserNoActivityTimeout: 100000,
+   		preprocessors: {
+      		'.tmp/scripts/**/*.js': 'coverage'
+   		},
+    	reporters: process.env['REPORTERS'] || ['coverage'],
+    	coverageReporter: {
+      		type: 'lcov',
+      		dir: '.tmp/report/coverage-report/'
+    	},
+    	plugins: [
+  			'karma-coverage',
+  			'karma-chrome-launcher',
+  			'karma-mocha',
+  			'karma-chai',
+  			'karma-sinon'
+		],
     	client: {
       		mocha:{
         	timeout: 15000
